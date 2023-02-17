2f9fbf060d9214a0fa4f77ba06793f7b148b4ce3
Arati1209 <47882905+Arati1209@users.noreply.github.com>
2023-02-17 12:50:17 +0530

    3 files modified

--- src/app/views/employee/add-employee/add-employee.component.ts
+++ src/app/views/employee/add-employee/add-employee.component.ts
@@ -1,38 +1,45 @@
-
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  styleUrls: ['./add-employee.component.css'],
 })
 export class AddEmployeeComponent implements OnInit {
   addInfForm!: FormGroup;
-  editId:any;
+  editId: any;
   submitted = false;
-  infoList:any=[];
-  flag=true;
+  infoList: any = [];
+  flag = true;
+  chePwd = false;
   constructor(
     private router: Router,
-    private datepipe:DatePipe,
-    private fb: FormBuilder,
-  ) { }
+    private datepipe: DatePipe,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.arr();
 
     this.addInfForm = this.fb.group({
-      fname: new FormControl('',
+      fname: new FormControl(
+        '',
 
         Validators.compose([
           Validators.required,
           Validators.pattern('[a-zA-Z ]*'),
         ])
       ),
-      lname: new FormControl('',
+      lname: new FormControl(
+        '',
 
         Validators.compose([
           Validators.required,
@@ -60,39 +67,37 @@ export class AddEmployeeComponent implements OnInit {
       password: new FormControl(
         '',
         Validators.compose([
-
           Validators.required,
           Validators.pattern(
             '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$'
           ),
         ])
       ),
-      confirmPwd: new FormControl(''),
-
-    })
+      confirmPwd: new FormControl('', Validators.required),
+    });
   }
 
   validation_msg = {
     fname: [
       {
         type: 'required',
-        message: 'First Name is required'
+        message: 'First Name is required',
       },
       {
         type: 'pattern',
-        message: 'First Name is string'
-      }
+        message: 'First Name is string',
+      },
     ],
-  
+
     lname: [
       {
         type: 'required',
-        message: 'Last Name is required'
+        message: 'Last Name is required',
       },
       {
         type: 'pattern',
-        message: 'Last Name is string'
-      }
+        message: 'Last Name is string',
+      },
     ],
     phone: [
       {
@@ -109,7 +114,7 @@ export class AddEmployeeComponent implements OnInit {
       },
       {
         type: 'required',
-        message: 'phone number  is required'
+        message: 'phone number  is required',
       },
     ],
     email: [
@@ -119,25 +124,25 @@ export class AddEmployeeComponent implements OnInit {
       },
       {
         type: 'required',
-        message: 'email number  is required'
+        message: 'email number  is required',
       },
     ],
     gender: [
       {
         type: 'required',
-        message: 'Gender is required'
+        message: 'Gender is required',
       },
     ],
     company: [
       {
         type: 'required',
-        message: 'Company Name is required'
+        message: 'Company Name is required',
       },
     ],
     dob: [
       {
         type: 'required',
-        message: 'Date of birth is required '
+        message: 'Date of birth is required ',
       },
     ],
     password: [
@@ -162,149 +167,120 @@ export class AddEmployeeComponent implements OnInit {
     confirmPwd: [
       {
         type: 'required',
-        message: 'Password is required.',
-      },
-      {
-        type: 'minlength',
-        message: 'Password must be at least 5 characters long.',
-      },
-      {
-        type: 'maxlength',
-        message: 'Password cannot be more than 16 characters long.',
-      },
-      {
-        type: 'pattern',
-        message:
-          'Your password must contain at least one uppercase,one special character, one lowercase, and one number.',
+        message: 'Confirm  Password is required',
       },
     ],
-  }
-  arr(){
-    this.infoList=[
+  };
+  arr() {
+    this.infoList = [
       {
-          "firstname": "Akhil",
-          "lastname": "Kumar",
-          "email": "akhil@gmail.com	",
-          "phone": 9959479459,
-          "company": "BuzzBoard",
-          "gender": "male",
-          "birthdate": "1991-03-14",
-          "password": "Abcd@123"
+        firstname: 'Akhil',
+        lastname: 'Kumar',
+        email: 'akhil@gmail.com	',
+        phone: 9959479459,
+        company: 'BuzzBoard',
+        gender: 'male',
+        birthdate: '1991-03-14',
+        password: 'Abcd@123',
       },
       {
-          "firstname": "Rahul",
-          "lastname": "Dev",
-          "email": "rahul@gmail.com",
-          "phone": 8923193993,
-          "company": "Infosys",
-          "gender": "female",
-          "birthdate": "1990-07-09",
-          "password": "Pqrs@123"
+        firstname: 'Rahul',
+        lastname: 'Dev',
+        email: 'rahul@gmail.com',
+        phone: 8923193993,
+        company: 'Infosys',
+        gender: 'female',
+        birthdate: '1990-07-09',
+        password: 'Pqrs@123',
       },
       {
-        "firstname": "Sampath",
-        "lastname": "Kumar",
-        "email": "sam@gmail.com",
-        "phone": 9703037744,
-        "company": "Cognizant",
-        "gender": "female",
-        "birthdate": "1989-06-27",
-        "password": "Xyz@1234"
-    }
-  ]
+        firstname: 'Sampath',
+        lastname: 'Kumar',
+        email: 'sam@gmail.com',
+        phone: 9703037744,
+        company: 'Cognizant',
+        gender: 'female',
+        birthdate: '1989-06-27',
+        password: 'Xyz@1234',
+      },
+    ];
   }
-  checkPassword(){
-    if(this.addInfForm.value.password!=this.addInfForm.value.confirmPwd)
-    {
-      alert('password does not match')
+  checkPassword() {
+    if (this.addInfForm.value.password != this.addInfForm.value.confirmPwd) {
+      this.chePwd = true;
+      alert('password does not match');
     }
-
   }
 
   submit() {
-    this.submitted=true;
-   if(this.addInfForm.valid)
-   {
-       const dt = new Date(this.addInfForm.value.dob);
-       const dob=this.datepipe.transform(dt,'yyyy-dd-MM')
-      let data = {
-        firstname: this.addInfForm.value.fname,
-        lastname: this.addInfForm.value.lname,
-        email: this.addInfForm.value.email,
-        phone: this.addInfForm.value.phone,
-        company: this.addInfForm.value.company,
-        gender: this.addInfForm.value.gender,
-        birthdate: this.addInfForm.value.dob,
-        password: this.addInfForm.value.password,
-         
-      }
-    
-        this.infoList.push(data)
+    this.submitted = true;
+    if (this.addInfForm.valid) {
+      if (this.chePwd == true) {
+        const dt = new Date(this.addInfForm.value.dob);
+        const dob = this.datepipe.transform(dt, 'yyyy-dd-MM');
+        let data = {
+          firstname: this.addInfForm.value.fname,
+          lastname: this.addInfForm.value.lname,
+          email: this.addInfForm.value.email,
+          phone: this.addInfForm.value.phone,
+          company: this.addInfForm.value.company,
+          gender: this.addInfForm.value.gender,
+          birthdate: this.addInfForm.value.dob,
+          password: this.addInfForm.value.password,
+          confirmPwd: this.addInfForm.value.confirmPwd,
+        };
+        this.infoList.push(data);
         this.addInfForm.reset();
-        console.log('dag',this.infoList);
-      
-
-   }
-   else{
-    alert('Please enter required and valid data')
-   }
-  
-  
+        console.log('dag', this.infoList);
+      } else {
+        alert('password does not match');
+      }
+    } else {
+      alert('Please enter required and valid data');
+    }
   }
-  edit(obj:any,i:any){
+  edit(obj: any, i: any) {
     console.log(i);
-    this.flag=false;
-    this.editId=i;
+    this.flag = false;
+    this.editId = i;
     this.addInfForm.controls['fname'].setValue(obj.firstname);
     this.addInfForm.controls['lname'].setValue(obj.lastname);
     this.addInfForm.controls['dob'].setValue(obj.birthdate);
     this.addInfForm.controls['email'].setValue(obj.email);
     this.addInfForm.controls['phone'].setValue(obj.phone);
-    if(obj.gender=='female')
-    this.addInfForm.controls['gender'].setValue('female');
-    else
-    this.addInfForm.controls['gender'].setValue('male');
+    if (obj.gender == 'female')
+      this.addInfForm.controls['gender'].setValue('female');
+    else this.addInfForm.controls['gender'].setValue('male');
     this.addInfForm.controls['company'].setValue(obj.company);
     this.addInfForm.controls['password'].setValue(obj.password);
     this.addInfForm.controls['confirmPwd'].setValue(obj.password);
- 
-
   }
-  delete(i:any){
+  delete(i: any) {
     this.infoList.splice(i, 1);
     this.infoList;
   }
   update() {
-    if(this.addInfForm.valid)
-    {
-        const dt = new Date(this.addInfForm.value.dob);
-        const dob=this.datepipe.transform(dt,'yyyy-dd-MM')
-       let data = {
-         firstname: this.addInfForm.value.fname,
-         lastname: this.addInfForm.value.lname,
-         email: this.addInfForm.value.email,
-         phone: this.addInfForm.value.phone,
-         company: this.addInfForm.value.company,
-         gender: this.addInfForm.value.gender,
-         birthdate: this.addInfForm.value.dob,
-         password: this.addInfForm.value.password,
-          
-       }
-    this.infoList[this.editId]=data;
+    const dt = new Date(this.addInfForm.value.dob);
+    const dob = this.datepipe.transform(dt, 'yyyy-dd-MM');
+    let data = {
+      firstname: this.addInfForm.value.fname,
+      lastname: this.addInfForm.value.lname,
+      email: this.addInfForm.value.email,
+      phone: this.addInfForm.value.phone,
+      company: this.addInfForm.value.company,
+      gender: this.addInfForm.value.gender,
+      birthdate: this.addInfForm.value.dob,
+      password: this.addInfForm.value.password,
+      confirmPwd: this.addInfForm.value.confirmPwd,
+    };
+    this.infoList[this.editId] = data;
     this.cancel();
-   
- }
- else{
-  alert('Please enter required and valid data')
- }
   }
-  cancel(){
-  this.addUser();
+  cancel() {
+    this.addUser();
   }
-  addUser()
-  {
+  addUser() {
     this.addInfForm.reset();
-    this.flag=true;
+    this.flag = true;
   }
-
 }
